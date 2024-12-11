29a57d4a91a2a5c46aa184e353b5bc721e7b88dc
JimCode90 <jimmypaytan10@gmail.com>
2024-12-11 18:18:54 -0500

    config ipserver production

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@ export const DB_PASSWORD = "4dm1n123456.";
 export const DB_HOST = "polipatrol.scyv6.mongodb.net";
 export const DB_APP_NAME = "PoliPatrol"
 
-export const IP_SERVER = "192.168.35.221";
+export const IP_SERVER = "0.0.0.0";
 
 export const JWT_SECRET_KEY = "gR7cH9Svfj8JLe4c186Ghs48hheb3902nh5DsA";
 
